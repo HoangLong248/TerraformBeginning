99074bfdafec1c0a4a00fe1003a9f6e947a795e2
Long.Tran <tranhuuhoanglong248@gmail.com>
2023-01-06 21:32:15 +0700

    finish section terraform file

--- main.tf
+++ main.tf
@@ -4,10 +4,36 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "my-first-server" {
-    ami = "ami-0b93ce03dcbcb10f6"
-    instance_type = "t2.micro"
-    
+resource "aws_vpc" "first-vpc" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "production"
+    }
+}
+
+resource "aws_vpc" "second-vpc" {
+    cidr_block = "10.1.0.0/16"
+    tags = {
+        Name = "dev"
+    }
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.first-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "prod-subnet"
+  }
+}
+
+resource "aws_subnet" "subnet-2" {
+  vpc_id     = aws_vpc.second-vpc.id
+  cidr_block = "10.1.1.0/24"
+
+  tags = {
+    Name = "dev-subnet"
+  }
 }
 
 # resource "<provider>_<resource_type>" "name" {
